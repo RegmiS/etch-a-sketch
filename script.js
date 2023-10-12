52eb73c74249b5bf7522835655dc4f89ba137ad6
RegmiS <sandeshreg35@gmail.com>
2023-10-12 16:40:42 -0500

    Remove unnecessary styling to ensure basic wrapping

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const main_container = document.querySelector(".container");
 
 for(let i=0; i<32; i++){
     const new_div = document.createElement('div');
-    new_div.textContent = `Test${i}`;
+    new_div.textContent = `Test${i+1}`;
     new_div.classList.add("item");
     main_container.appendChild(new_div);
 }
